8760169c56604cb2f166f1e4a59815e29f790bf4
colinjones0 <colin_jones@brown.edu>
2022-11-11 15:57:20 -0500

    handin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,34 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+
+  function addItem(item) {
+    setCart([...cart, item]);
+  }
+
+  function calc() {
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+      total += cart[i].price;
+    }
+    return total;
+  }
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Jesse, We Need to Bake</h1>
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <p>
+          <BakeryItem item= {item} name={item.name} description={item.description} price={item.price} image={item.image} addItem={addItem}/>
+        </p> 
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+          {cart.map((item, index) => (<p>{item.name}</p>))}
+          Total: {calc()}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,13 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem({item, name, description, price, image, addItem}) {
+    return (
+        <div> 
+        <h3>{name}</h3> 
+        <p>{description}</p>
+        <p>{price}</p>
+        <img className="item-image" src={image} alt={"item image"}/>
+        <button onClick={() => {addItem(item)}}> Add to Cart </button>
+        </div>
+    );
+}
